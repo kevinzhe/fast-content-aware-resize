3312d7d86b7cc7039fceb9d043343e417648014e
Kevin Zheng <kevinzhe@andrew.cmu.edu>
2018-11-06 14:25:29 -0500

    Update timing harness to do multiple trials

--- fastcode_pics/script.py
+++ fastcode_pics/script.py
@@ -2,6 +2,8 @@ import subprocess
 
 from PIL import Image
 
+TRIALS = 16
+
 def bench(bin_file):
     output = subprocess.run(["ls"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     for pic in output.split('\n'):
@@ -12,21 +14,26 @@ def bench(bin_file):
         im = Image.open(pic)
         width, height = im.size
 
-        for num_seams in range(1, width//2, 20):
-            times = [bin_file.split('/')[-1], str(width), str(height), str(num_seams)]
-            output = subprocess.run([bin_file, pic, "/dev/null", str(num_seams)], 
+        interval = max(width // 32, 1)
+
+        for num_seams in range(1, width//2, interval):
+            output = subprocess.run([bin_file, pic, "/dev/null", str(num_seams), str(TRIALS)], 
                 stderr=subprocess.PIPE).stderr.decode('utf-8')    
             
-            properties = output[output.find("grey"):]
-            for prop in properties.split('\n'):
-                if "Completed in" in prop:
-                    break
-                idx = prop.find("\t")
-                time_str = prop[idx+1:]
-                end = time_str.find("\t")
-                time = time_str[:end]
-                times.append(time)
-            print(','.join(times))
+            for trial in output.split('Running iteration'):
+              times = [bin_file.split('/')[-1], str(width), str(height), str(num_seams)]
+              prop_start = trial.find('grey')
+              if prop_start == -1: continue
+              properties = trial[prop_start:]
+              for prop in properties.split('\n'):
+                  if "Completed in" in prop:
+                      break
+                  idx = prop.find("\t")
+                  time_str = prop[idx+1:]
+                  end = time_str.find("\t")
+                  time = time_str[:end]
+                  times.append(time)
+              print(','.join(times))
 
 hdr = 'testname, width, height, num_seams_removed, grey, conv, convp, pathsum, minpath, rmpath, malloc, total'
 print(','.join(hdr.split(', ')))
